244574ac49bed301e132f7aff9fd229d200b75b9
luismoralesp <luismiguel.mopa@gmail.com>
2016-06-10 08:52:27 -0500

    mongo

--- listening.js
+++ listening.js
@@ -1,113 +1,128 @@
+var mongoose = require('mongoose');
 
 module.exports = {
 	listenings : {},
 
-	get_webusers: function(type){
-		return this.listenings[type];
+	setup: function (callback){
+		mongoose.connect('mongodb://localhost/test');
+		var db = mongoose.connection;
+		db.on('error', console.error.bind(console, 'connection error:'));
+		db.once('open', function (){
+			var message = mongoose.Schema({
+				'type': String,
+				'webuser': String,
+				'data': Object,
+				'visited': Boolean
+			});
+			this.Message = mongoose.model('Message', message);
+			var session = mongoose.Schema({
+				'type': String,
+				'webuser': String,
+				'sessions': [
+					{
+						'socket_id': String,
+						'session_id': String,
+					}
+				]
+			});
+			this.Session = mongoose.model('Session', session);
+
+			callback();
+		}.bind(this));
+	},
+
+	reset: function(){
+		this.Session.remove({}).exec();
+		this.Message.remove({}).exec();
 	},
 
 	add_messages_by_type: function (type, messages, callback){
-		if (this.listenings[type] == undefined){
-			this.listenings[type] = {};
-		}
-		for (var webuser in this.listenings[type]){
-			if (this.listenings[type][webuser] == undefined){
-				this.listenings[type][webuser] = {'sessions': {}, 'messages': [] };
-			}
-
-			for (var i in messages) {
-				var message_id = this.listenings[type][webuser]['messages'].length.toString(16);
-				messages[i]['message_id'] = message_id;
-				messages[i]['_visited_'] = [];
-				this.listenings[type][webuser]['messages'][message_id] = messages[i];
-				for (var django_id in this.listenings[type][webuser]['sessions']){
-					var socket = this.listenings[type][webuser]['sessions'][django_id];
-					callback(django_id, socket, messages[i]);
+		this.Session.find({'type': type}, {}, function(err, raw){
+			console.log(raw);
+			raw.forEach(function (doc, index, raw) {
+				for (var i in messages) {
+					var message = new this.Message({
+						'type': type,
+						'webuser': doc.webuser,
+						'data': messages[i],
+						'visited': false
+					});
+					message.save();
+					for (var j in doc.sessions){
+						callback(doc.sessions[j].session_id, doc.sessions[j].socket_id, message);
+					}
 				}
-			}
-		}
+			}.bind(this));
+		}.bind(this));
 	},
 
 	add_messages: function (type, webuser, messages){
-		if (this.listenings[type] == undefined){
-			this.listenings[type] = {};
-		}
-		if (this.listenings[type][webuser] == undefined){
-			this.listenings[type][webuser] = {'sessions': {}, 'messages': []};
-		}
-
 		for (var i in messages){
-			var message_id = this.listenings[type][webuser]['messages'].length.toString(16);
-			messages[i]['message_id'] = message_id;
-			messages[i]['_visited_'] = [];
-			this.listenings[type][webuser]['messages'][message_id] = messages[i];
+			var message = new this.Message({'type': type, 'webuser': webuser, 'data': messages[i], 'visited': false});
+			message.save();
 		}
 	},
 
 	visit_message: function (type, webuser, message_id, session_id, callback){
-		this.listenings[type][webuser]['messages'][message_id]['_visited_'].push(session_id);
-
-		for (var session_id in this.listenings[type][webuser]['sessions']){
-			callback(session_id, this.listenings[type][webuser]['sessions'][session_id], message_id);
-		}
+		this.Message.update(
+			{'_id': message_id},
+			{'visited': true},
+			function(err, doc){
+				this.Session.findOne({'type': type, 'webuser':webuser}, function(err, doc){
+					callback(err, doc);
+				});
+			}.bind(this)
+		);
 	},
 
-	add_session: function (type, webuser, session, socket){
-		if (this.listenings[type] == undefined){
-			this.listenings[type] = {};
-		}
-		if (this.listenings[type][webuser] == undefined){
-			this.listenings[type][webuser] = {'sessions': {}, 'messages': []};
-		}
-		if (this.listenings[type][webuser]['sessions'][session] == undefined){
-			this.listenings[type][webuser]['sessions'][session] = {};
-		}
-
-		this.listenings[type][webuser]['sessions'][session][socket.id] = socket;
+	add_session: function (type, webuser, session_id, socket_id, callback){
+		var session = this.Session.find({'type': type, 'webuser': webuser}, function(err, raw){
+			if (raw.length == 0){
+				var session = new this.Session({
+					'type': type,
+					'webuser': webuser,
+					'sessions': [
+						{'session_id': session_id, 'socket_id': socket_id}
+					]
+				});
+				session.save(function (){
+					if (callback){
+						callback(session);
+					}
+				});
+			}else{
+				this.Session.update(
+					{'type': type, 'webuser': webuser}, 
+					{
+						$push: {'sessions': {'session_id': session_id, 'socket_id': socket_id}}
+					}
+				).exec(function (){
+					if (callback){
+						callback(session);
+					}
+				});
+			}
+		}.bind(this));
+		
 	},
 
-	get_messages: function (type, webuser){
-		if (this.listenings[type] == undefined){
-			this.listenings[type] = {};
-		}
-		if (this.listenings[type][webuser] == undefined){
-			this.listenings[type][webuser] = {'sessions': {}, 'messages': []};
-		}
-
-		return this.listenings[type][webuser]['messages'];
+	get_messages: function (type, webuser, callback){
+		this.Message.find({'type': type, 'webuser': webuser, 'visited': false}, callback);
 	},
 
-	get_sessions: function (type, webuser){
-		if (this.listenings[type] == undefined){
-			this.listenings[type] = {};
-		}
-		if (this.listenings[type][webuser] == undefined){
-			this.listenings[type][webuser] = {'sessions': {}, 'messages': []};
-		}
-
-		return this.listenings[type][webuser]['sessions'];
+	get_sessions: function (type, webuser, callback){
+		this.Session.find({'type': type, 'webuser':webuser}, callback);
 	},
 
-	delete_messages: function (type, webuser, message_id){
-		if (this.listenings[type] == undefined){
-			this.listenings[type] = {};
-		}
-		if (this.listenings[type][webuser] == undefined){
-			this.listenings[type][webuser] = {'sessions': {}, 'messages': []};
-		}
-
-		delete this.listenings[type][webuser]['messages'][message_id];
+	delete_message: function (message_id){
+		this.Message.remove({'_id': message_id}).exec();
 	},
 
-	delte_session: function (type, webuser, session, socket_id){
-		if (this.listenings[type] == undefined){
-			this.listenings[type] = {};
-		}
-		if (this.listenings[type][webuser] == undefined){
-			this.listenings[type][webuser] = {'sessions': {}, 'messages': []};
-		}
-
-		delete this.listenings[type][webuser]['sessions'][session][socket_id];
+	delete_session: function (type, webuser, session_id, socket_id){
+		this.Session.update(
+			{'type': type, 'webuser': webuser}, 
+			{$pull: {'sessions': {'session_id': session_id, 'socket_id': socket_id}}}
+		).exec();
 	}
 
 };
--- test.js
+++ test.js
@@ -1,18 +1,16 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-	console.log('we are connected');
-	var schema = mongoose.Schema({
-	    name: Object
-	});
-	var Model1 = mongoose.model('model1', schema);
-	//Model1.find({}).remove({}).exec();
-	//var model1 = new Model1({'name': 'hola'});
-	//model1.save();
-	Model1.update({'name': 'hola'}, {'name': {'hola':'mundo'}}).exec();
-	Model1.find({}, function (err, data){
-		console.log(data);
+var listening = require('./listening');
+
+listening.setup(function (){
+	listening.reset();
+	listening.add_session('User', 'admin', 'session_id1', 'socket_id1', function(session){
+		listening.add_messages_by_type('User', {'message': 'message1'}, function(django_id, socket_id, message){
+			console.log(django_id, socket_id, message);
+			listening.visit_message(message._id, function(){
+				listening.delete_message(message._id);
+				listening.delete_session(session.type, session.webuser, session.sessions[0].session_id, session.sessions[0].socket_id);
+			})
+		});
 	});
 });
+
+
